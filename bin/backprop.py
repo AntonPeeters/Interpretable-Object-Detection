fixed backprop

bin/backprop.py
@@ -25,7 +25,7 @@ class VanillaBackprop:
         first_layer = list(self.model.layers._modules.items())[0][1][0].layers[0]
         first_layer.register_backward_hook(hook_function)
 
-    def generate_gradients(self, params, img_tf, annos, device):
+    def generate_gradients(self, params, img_tf, annos, device, class_flag=True, box_flag=True):
         output = self.model(img_tf.to(device))
 
         # Tensor
@@ -39,7 +39,6 @@ class VanillaBackprop:
         w = output.size(3)
         anchors = torch.Tensor(self.model.anchors)
         num_anchors = anchors.shape[0]
-        conf_thresh = 0.5
 
         # Compute xc,yc, w,h, box_score on Tensor
         lin_x = torch.linspace(0, w - 1, w).repeat(h, 1).view(h * w).to(device)
@@ -57,31 +56,8 @@ class VanillaBackprop:
         network_output[:, :, 3, :].exp_().mul_(anchor_h).div_(h)  # Height
         network_output[:, :, 4, :].sigmoid_()  # Box score
 
-        # Compute class_score
-        if len(params.class_label_map) > 1:
-            with torch.no_grad():
-                cls_scores = torch.nn.functional.softmax(network_output[:, :, 5:, :], 2)
-            cls_max, cls_max_idx = torch.max(cls_scores, 2)
-            cls_max_idx = cls_max_idx.float()
-            cls_max.mul_(network_output[:, :, 4, :])
-        else:
-            cls_max = network_output[:, :, 4, :]
-            cls_max_idx = torch.zeros_like(cls_max)
-
-        score_thresh = cls_max > conf_thresh
-
-        # Mask select boxes > conf_thresh
-        coords = network_output.transpose(2, 3)[..., 0:4]
-        coords = coords[score_thresh[..., None].expand_as(coords)].view(-1, 4)
-        scores = cls_max[score_thresh]
-        idx = cls_max_idx[score_thresh]
-
-        # Get batch numbers of the detections
-        batch_num = score_thresh.view(batch, -1)
-        nums = torch.arange(1, batch + 1, dtype=torch.uint8, device=batch_num.device)
-        batch_num = (batch_num * nums[:, None])[batch_num] - 1
-
-        #print(torch.cat([batch_num[:, None].float(), coords, scores[:, None], idx[:, None]], dim=1))
+        gradients_as_arr = []
+        gradients_as_ten = []
 
         for idx, entry in annos.iterrows():
             # Variable
@@ -100,14 +76,31 @@ class VanillaBackprop:
             # Reform output
             out = output.clone()
             out = out.view(batch, num_anchors, -1, h, w)
-            out = out[:, :, 5:, :, :]
-            out[:, :, 4, :, :].sigmoid()
 
-            # Compute gradient
-            one_hot_output = torch.zeros_like(out)
-            one_hot_output[0, anchor_max_idx, 0, grid_y, grid_x] = 1
-            one_hot_output[0, anchor_max_idx, cls_index, grid_y, grid_x] = 1
-            out[~one_hot_output.bool()] = 0
+            # Box only
+            if box_flag is True and class_flag is False:
+                out = out[:, :, 4, :, :]
+                # Compute gradient
+                one_hot_output = torch.zeros_like(out)
+                one_hot_output[0, anchor_max_idx, grid_y, grid_x] = 1
+                out[~one_hot_output.bool()] = 0
+
+            # Class only
+            if box_flag is False and class_flag is True:
+                out = out[:, :, 5:, :, :]
+                # Compute gradient
+                one_hot_output = torch.zeros_like(out)
+                one_hot_output[0, anchor_max_idx, cls_index, grid_y, grid_x] = 1
+                out[~one_hot_output.bool()] = 0
+
+            # Box and class
+            if box_flag is True and class_flag is True:
+                out = out[:, :, 4:, :, :]
+                # Compute gradient
+                one_hot_output = torch.zeros_like(out)
+                one_hot_output[0, anchor_max_idx, 0, grid_y, grid_x] = 1
+                one_hot_output[0, anchor_max_idx, cls_index, grid_y, grid_x] = 1
+                out[~one_hot_output.bool()] = 0
 
             # Zero grads
             self.model.zero_grad()
@@ -117,21 +110,25 @@ class VanillaBackprop:
 
             # Convert Pytorch variable to numpy array
             # [0] to get rid of the first channel (1,3,224,224)
-            gradients_as_arr = self.gradients.data.cpu().numpy()[0]
+            gradients_as_ten.append(self.gradients[0])
+            gradients_as_arr.append(self.gradients.data.cpu().numpy()[0])
 
-            # Save colored gradients
-            save_gradient_images(gradients_as_arr, entry.class_label + '_' + str(idx) + '_color')
-
-            # Convert to grayscale
-            grayscale_vanilla_grads = convert_to_grayscale(gradients_as_arr)
-
-            # Save grayscale gradients
-            save_gradient_images(grayscale_vanilla_grads, entry.class_label + '_' + str(idx) + '_gray')
+        torch.save(gradients_as_ten, "data/results/raw/tensor.pt")
+        return gradients_as_arr
 
 
 def backprop(params, img_tf, annos, device):
     # Vanilla backprop
     VBP = VanillaBackprop(params, device)
     # Generate gradients
-    VBP.generate_gradients(params, img_tf, annos, device)
+    gradients_as_arr = VBP.generate_gradients(params, img_tf, annos, device)
+
+    # Save colored gradients
+    save_gradient_images(gradients_as_arr, '_color')
+
+    # Convert to grayscale
+    grayscale_vanilla_grads = convert_to_grayscale(gradients_as_arr)
+
+    # Save grayscale gradients
+    save_gradient_images(grayscale_vanilla_grads, '_gray')
     print('Vanilla backprop completed')

bin/grad.py
@@ -112,8 +112,8 @@ class GradCam:
             # Reform output
             out = output.clone()
             out = out.view(batch, num_anchors, -1, height, width)
-            out = out[:, :, 5:, :, :]
-            out[:, :, 4, :, :].sigmoid()
+            out = out[:, :, 4:, :, :]
+            #out[:, :, 4, :, :].sigmoid_()
 
             # Compute gradient
             one_hot = np.zeros_like(out.clone().cpu().detach(), dtype=np.float32)

bin/gradcam.py
@@ -91,8 +91,8 @@ class GradCam:
             out = model_output.clone()
             conv_out = conv_output.clone()
             out = out.view(batch, num_anchors, -1, height, width)
-            out = out[:, :, 5:, :, :]
-            out[:, :, 4, :, :].sigmoid()
+            out = out[:, :, 4:, :, :]
+            #out[:, :, 4, :, :].sigmoid_()
 
             # Compute gradient
             one_hot_output = torch.zeros_like(out)

bin/misc_functions.py
@@ -24,29 +24,37 @@ def convert_to_grayscale(im_as_arr):
     returns:
         grayscale_im (numpy_arr): Grayscale image with shape (1,W,D)
     """
-    grayscale_im = np.sum(np.abs(im_as_arr), axis=0)
+    grayscale_im = np.sum(np.abs(im_as_arr), axis=1)
+    """grayscale_im = np.sum(np.abs(im_as_arr), axis=1)
     im_max = np.percentile(grayscale_im, 99)
     im_min = np.min(grayscale_im)
     grayscale_im = (np.clip((grayscale_im - im_min) / (im_max - im_min), 0, 1))
-    grayscale_im = np.expand_dims(grayscale_im, axis=0)
+    grayscale_im = np.expand_dims(grayscale_im, axis=1)"""
     return grayscale_im
 
 
-def save_gradient_images(gradient, file_name):
+def save_gradient_images(gradient_images, file_name):
     """
         Exports the original gradient image
 
     Args:
-        gradient (np arr): Numpy array of the gradient with shape (3, 224, 224)
+        gradient_images (np arr): Numpy arrays of the gradients with shape (3, 224, 224)
         file_name (str): File name to be exported
     """
-    # Normalize
-    gradient = gradient - gradient.min()
-    gradient /= gradient.max()
-    # Save image
+    np.save("data/results/raw/" + file_name, gradient_images)
+    i = 0
+    print("max:", np.array(gradient_images).max(), ", min:", np.array(gradient_images).min())
+    all_grad = gradient_images - np.array(gradient_images).min()
+    for gradient in gradient_images:
+        # Normalize
+        gradient = gradient - np.array(gradient_images).min()
+        gradient /= np.array(all_grad).max()
+        print(gradient.max(), gradient.min())
 
-    path_to_file = os.path.join('data/results/back', file_name + '.jpg')
-    save_image(gradient, path_to_file)
+        # Save image
+        path_to_file = os.path.join('data/results/back', str(i) + file_name + '.jpg')
+        save_image(gradient, path_to_file)
+        i += 1
 
 
 def save_class_activation_images(org_img, activation_map, file_name):